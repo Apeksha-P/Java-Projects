6023b18b1128bcfc5a6de3dff7d3d42941bce40c
Apeksha60 <pavithriapeksha1new@gmail.com>
2023-09-23 17:16:58 +0530

    My first Java programme

---
+++ src/helloWorld.java
@@ -0,0 +1,13 @@
+public class helloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World! ");
+        System.out.println("Hi");
+        System.out.print("Pavithri ");
+        System.out.print("Apeksha");
+        //write comment
+        //This is single line comment
+        /*This is
+        multi line comment
+         */
+    }
+}
