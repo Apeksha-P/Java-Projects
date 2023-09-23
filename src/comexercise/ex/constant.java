4187e8debd342bdb3e23e4c0578900ab5d6cb1b0
Apeksha60 <pavithriapeksha1new@gmail.com>
2023-09-23 23:52:00 +0530

    Constant variable

---
+++ src/comexercise/ex/constant.java
@@ -0,0 +1,8 @@
+package comexercise.ex;
+
+public class constant {
+    public static void main(String[] args) {
+        final String COMPANY_NAME = "Neso";
+        System.out.println(COMPANY_NAME);
+    }
+}
