a1c714c019240e17700d7a950543e5e7afe336dd
Apeksha60 <pavithriapeksha1new@gmail.com>
2023-09-23 23:38:00 +0530

    Java Exercise

---
+++ src/comexercise/ex/Variable.java
@@ -0,0 +1,11 @@
+package comexercise.ex;
+
+public class Variable {
+    public static void main(String[] args) {
+        String myName;      //Declaration
+        myName = "Apeksha";     //Assignment
+        String myJob = "Programmer";     //Initialization
+        System.out.println(myName);
+        System.out.println(myJob);
+    }
+}
