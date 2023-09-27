cfa63a7ef82b92cfbe1da60d9375f6a47e28fda7
Apeksha60 <pavithriapeksha1new@gmail.com>
2023-09-27 18:03:20 +0530

    Calling some method

---
+++ src/comexercise/ex/string.java
@@ -0,0 +1,8 @@
+package comexercise.ex;
+
+public class string {
+    public static void main(String[] args) {
+        String text = " This is a sentence.";
+        System.out.println(text.toUpperCase());
+    }
+}
