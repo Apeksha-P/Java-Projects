4617faaa1602b1e941ac7157de9b65cd41958b68
Apeksha60 <pavithriapeksha1new@gmail.com>
2023-10-03 21:32:17 +0530

    Concatenation

--- src/comexercise/ex/string.java
+++ src/comexercise/ex/string.java
@@ -6,6 +6,9 @@ public class string {
         System.out.println(text.toUpperCase());
         System.out.println(text.toLowerCase());
         System.out.println(text.length());
-        System.out.println(text.charAt(5));
+        char c = text.charAt(6);
+        System.out.println(c);
+        System.out.println(text.indexOf('t'));
+        System.out.println(text.concat("Apeksha"));
     }
 }
