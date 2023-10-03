8550470bd621a25cad0283bf84de47a12d3cb9cc
Apeksha60 <pavithriapeksha1new@gmail.com>
2023-10-03 21:12:42 +0530

    Calling Methods

--- src/comexercise/ex/string.java
+++ src/comexercise/ex/string.java
@@ -4,5 +4,8 @@ public class string {
     public static void main(String[] args) {
         String text = " This is a sentence.";
         System.out.println(text.toUpperCase());
+        System.out.println(text.toLowerCase());
+        System.out.println(text.length());
+        System.out.println(text.charAt(5));
     }
 }
