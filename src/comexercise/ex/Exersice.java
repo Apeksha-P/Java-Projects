3a78bec097f1ebcf2fa4c5315b676e982e088616
Apeksha60 <pavithriapeksha1new@gmail.com>
2023-10-08 20:22:53 +0530

    Exercise 1

---
+++ src/comexercise/ex/Exersice.java
@@ -0,0 +1,12 @@
+package comexercise.ex;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Exersice {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your favorite number: ");
+        System.out.println("Your favorite number is "+input.nextInt());
+    }
+}
