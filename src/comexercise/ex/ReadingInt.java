7f90edd1e2a04f886dccde708609ab073060d418
Apeksha60 <pavithriapeksha1new@gmail.com>
2023-10-08 17:34:58 +0530

    Reading input form keyboard

---
+++ src/comexercise/ex/ReadingInt.java
@@ -0,0 +1,10 @@
+package comexercise.ex;
+
+import java.util.Scanner;
+
+public class ReadingInt {
+    public static void main(String[] args) {
+        Scanner input  = new Scanner(System.in);
+        System.out.println(input.nextInt()+10);
+    }
+}
