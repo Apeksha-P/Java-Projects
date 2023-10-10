81257bf6a84213405794554e6098b79ce4b5a53e
Apeksha60 <pavithriapeksha1new@gmail.com>
2023-10-10 14:30:26 +0530

    Exercise 1

---
+++ src/SampleWindow.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class SampleWindow {
+    public static void main(String[] args) {
+        JFrame myWindow;
+        myWindow = new JFrame();
+        myWindow.setSize(300, 200);
+        myWindow.setTitle("My Window");
+        myWindow.setVisible(true);
+
+    }
+}
